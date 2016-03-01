709e127872a6db56cd14154accd67bc2c6e9a9b4
Francesco Abbate <a-fabbate@hotels.com>
2016-03-01 16:54:36 +0000

    added gitignore and removed out folder

--- src/core/MazeSolverImpl.java
+++ src/core/MazeSolverImpl.java
@@ -5,6 +5,8 @@ public class MazeSolverImpl implements MazeSolver {
 
     @Override
     public boolean canMoveBetweenPointsOnMaze(Maze maze, int startX, int startY, int endX, int endY) {
+
         //Implement your method here
+
     }
 }
